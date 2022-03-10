e58c427020e18f7d4c1507e5ce60a4f0e5a9f096
Praba <svinupraba@gmail.com>
2022-03-11 00:24:38 +0530

    abi

---
+++ src/org/Datatype/Training.java
@@ -0,0 +1,34 @@
+package org.Datatype;
+
+public class Training {
+	public void add() {
+byte age= 127;
+short app= 32456;
+int emp=123456789;
+long mob=9710658088l;
+float salary=45000.6574f;
+double cpay=75000.98765432;
+char gender='m';
+boolean status= false;
+String name = "Srivatsan";
+System.out.println(age);
+System.out.println(app);
+System.out.println(emp);
+System.out.println(mob);
+System.out.println(salary);
+System.out.println(cpay);
+System.out.println(gender);
+System.out.println(status);
+System.out.println(name);
+System.out.println("Srivatsan age is "+ age);
+System.out.println("My son name is " + name);
+System.out.println("My frient employee code number is " + emp);
+System.out.println("My age is " + age + " and application number is " + app + " and my employee code number is " + emp);
+
+	}
+	public static void main(String[] args) {
+		Training C=new Training();
+		C.add();
+	}
+
+}
